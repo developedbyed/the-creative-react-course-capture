e1657c77d3abda4ad526ef46681c25578b4a5596
developedbyed <ed.magician@gmail.com>
2020-10-19 19:57:57 +0300

    12.Framer Motion

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,26 +1,26 @@
 import React from "react";
 import home1 from "../img/home1.png";
-//Styled
-import styled from "styled-components";
 import { About, Description, Image, Hide } from "../styles";
+//Framer Motion
+import { motion } from "framer-motion";
 
 const AboutSection = () => {
   return (
     <About>
       <Description>
-        <div className="title">
+        <motion.div>
           <Hide>
-            <h2>We work to make</h2>
+            <motion.h2>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <h2>
+            <motion.h2>
               your <span>dreams</span> come
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>true.</h2>
+            <motion.h2>true.</motion.h2>
           </Hide>
-        </div>
+        </motion.div>
         <p>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.

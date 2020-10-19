a29704e35f2e32b246409c8c816910a3e69a53b9
developedbyed <ed.magician@gmail.com>
2020-10-19 22:09:16 +0300

    18.Finishing FAQ Animation

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { About } from "../styles";
 import Toggle from "./Toggle";
+import { AnimateSharedLayout } from "framer-motion";
 
 const FaqSection = () => {
   return (
@@ -9,9 +10,8 @@ const FaqSection = () => {
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
-      <Toggle>
-        <div className="question">
-          <h4>How Do I Start?</h4>
+      <AnimateSharedLayout>
+        <Toggle title="How Do I Start?">
           <div className="answer">
             <p>Lorem ipsum dolor sit amet.</p>
             <p>
@@ -20,40 +20,35 @@ const FaqSection = () => {
               magnam placeat corporis.
             </p>
           </div>
-          <div className="faq-line"></div>
-        </div>
-      </Toggle>
-
-      <div className="question">
-        <h4>Daily Schedule</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-
-      <div className="question">
-        <h4>Diferrent Payment Methods</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Necessitatibus, neque.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>What Products do you offer.</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Necessitatibus, neque.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
+        </Toggle>
+        <Toggle title="Daily Schedule">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error,
+              totam.
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="Diferrent Payment Methods">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Necessitatibus, neque.
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="What Products do you offer.">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Necessitatibus, neque.
+            </p>
+          </div>
+        </Toggle>
+      </AnimateSharedLayout>
     </Faq>
   );
 };
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,8 +1,15 @@
+import { motion } from "framer-motion";
 import React, { useState } from "react";
 
-const Toggle = ({ children }) => {
+const Toggle = ({ children, title }) => {
   const [toggle, setToggle] = useState(true);
-  return <div onClick={() => setToggle(!toggle)}>{toggle ? children : ""}</div>;
+  return (
+    <motion.div layout className="question" onClick={() => setToggle(!toggle)}>
+      <motion.h4 layout>{title}</motion.h4>
+      {toggle ? children : ""}
+      <div className="faq-line"></div>
+    </motion.div>
+  );
 };
 
 export default Toggle;
